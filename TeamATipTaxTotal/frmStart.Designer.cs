3f0084cf8b623774ab8f0f1b891712d06e463c97
zbiggy1982 <wmitchell1982@gmail.com>
2014-01-14 15:45:40 -0700

    commit

---
+++ TeamATipTaxTotal/frmStart.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace TeamATipTaxTotal
+{
+    partial class frmStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbFoodCharge = new System.Windows.Forms.TextBox();
+            this.lblEnterTotal = new System.Windows.Forms.Label();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.tbTax = new System.Windows.Forms.TextBox();
+            this.tbTip = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.ttInfo = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // tbFoodCharge
+            // 
+            this.tbFoodCharge.Location = new System.Drawing.Point(114, 33);
+            this.tbFoodCharge.Name = "tbFoodCharge";
+            this.tbFoodCharge.Size = new System.Drawing.Size(140, 20);
+            this.tbFoodCharge.TabIndex = 1;
+            // 
+            // lblEnterTotal
+            // 
+            this.lblEnterTotal.AutoSize = true;
+            this.lblEnterTotal.Location = new System.Drawing.Point(12, 40);
+            this.lblEnterTotal.Name = "lblEnterTotal";
+            this.lblEnterTotal.Size = new System.Drawing.Size(96, 13);
+            this.lblEnterTotal.TabIndex = 3;
+            this.lblEnterTotal.Text = "Enter Food Charge";
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.Location = new System.Drawing.Point(86, 73);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(22, 13);
+            this.lblTip.TabIndex = 4;
+            this.lblTip.Text = "Tip";
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.Location = new System.Drawing.Point(83, 106);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(25, 13);
+            this.lblTax.TabIndex = 5;
+            this.lblTax.Text = "Tax";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(77, 139);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "Total";
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.Location = new System.Drawing.Point(114, 132);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.ReadOnly = true;
+            this.tbTotal.Size = new System.Drawing.Size(140, 20);
+            this.tbTotal.TabIndex = 7;
+            // 
+            // tbTax
+            // 
+            this.tbTax.Location = new System.Drawing.Point(114, 99);
+            this.tbTax.Name = "tbTax";
+            this.tbTax.ReadOnly = true;
+            this.tbTax.Size = new System.Drawing.Size(140, 20);
+            this.tbTax.TabIndex = 8;
+            // 
+            // tbTip
+            // 
+            this.tbTip.Location = new System.Drawing.Point(114, 66);
+            this.tbTip.Name = "tbTip";
+            this.tbTip.ReadOnly = true;
+            this.tbTip.Size = new System.Drawing.Size(140, 20);
+            this.tbTip.TabIndex = 9;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 177);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(98, 177);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(179, 177);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // ttInfo
+            // 
+            this.ttInfo.IsBalloon = true;
+            // 
+            // frmStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 240);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.tbTip);
+            this.Controls.Add(this.tbTax);
+            this.Controls.Add(this.tbTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.lblTip);
+            this.Controls.Add(this.lblEnterTotal);
+            this.Controls.Add(this.tbFoodCharge);
+            this.Name = "frmStart";
+            this.Text = "Tip Calculator";
+            this.Load += new System.EventHandler(this.frmStart_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFoodCharge;
+        private System.Windows.Forms.Label lblEnterTotal;
+        private System.Windows.Forms.Label lblTip;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox tbTotal;
+        private System.Windows.Forms.TextBox tbTax;
+        private System.Windows.Forms.TextBox tbTip;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ToolTip ttInfo;
+
+    }
+}
+
---
+++ TeamATipTaxTotal/frmStart.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeamATipTaxTotal
+{
+    //Team A In Class Excercise Week 1
+    //Tip, Tax, Total App
+    //Version 1
+    //William Mitchell, Auric Beihl, Dennis Chapman
+    //December 19, 2013
+    //POS_409
+    //Dr Brad Purdy
+
+    public partial class frmStart : Form
+    {
+        //declares constant values for tax and tip percentages
+        public const double TAX_PERCENTAGE = .07;
+        public const double TIP_PERCENTAGE = .15;
+        
+        
+        public frmStart()
+        {
+            InitializeComponent();
+        }
+
+        //exits app
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //resets form
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            this.tbFoodCharge.Clear();
+            this.tbTax.Clear();
+            this.tbTip.Clear();
+            this.tbTotal.Clear();
+            this.tbFoodCharge.Focus();
+
+        }
+
+        //calculates the amounts
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            this.tbTax.Clear();
+            this.tbTip.Clear();
+            this.tbTotal.Clear();
+            decimal decimalFoodCharge; 
+            decimal decimalTipCharge;
+            decimal decimalTaxCharge;
+            decimal decimalTotal;
+
+            //conditional test for parsibility
+            if (decimal.TryParse(this.tbFoodCharge.Text, out decimalFoodCharge))
+            {
+                decimalTaxCharge = decimalFoodCharge * (decimal)TAX_PERCENTAGE;
+                decimalTipCharge = decimalFoodCharge * (decimal)TIP_PERCENTAGE;
+                decimalTotal = decimalFoodCharge + decimalTipCharge + decimalTaxCharge;
+                this.tbTax.Text = decimalTaxCharge.ToString("C");
+                this.tbTip.Text = decimalTipCharge.ToString("C");
+                this.tbTotal.Text = decimalTotal.ToString("C");
+                
+            }
+                //diplays error
+            else
+            {
+                MessageBox.Show("Please Enter A Numeric Value for The Food Charge", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //sets tool tip on form load
+        private void frmStart_Load(object sender, EventArgs e)
+        {
+            this.ttInfo.SetToolTip(this.tbFoodCharge, "Enter The Food Charge Amount");
+            this.ttInfo.SetToolTip(this.btnExit, "Exit Application");
+            this.ttInfo.SetToolTip(this.btnCalculate, "Calculate the Tax, Tip, and Total");
+            this.ttInfo.SetToolTip(this.btnReset, "Reset the form");
+            this.ttInfo.SetToolTip(this.tbTax, "Displays the Tax Total");
+            this.ttInfo.SetToolTip(this.tbTip, "Displays the Tip Total");
+            this.ttInfo.SetToolTip(this.tbTotal, "Displays the Total");
+
+        }
+    }
+}
